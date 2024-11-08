0b04b61f3fe6916b5135c2dcf0015b9e804bc23a
Promisekel <101825563+Promisekel@users.noreply.github.com>
2024-11-08 17:06:26 +0000

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ st.set_page_config(layout="wide")
 
 @st.cache_data
 def get_tesla_data():
-    df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/tesla-stock-price.csv')
+    df = pd.read_csv('/kaggle/input/patients-data-for-medical-field/Patients Data ( Used for Heart Disease Prediction ).xlsx')
     df = df.drop(0)
     df['volume'] = df['volume'].astype(float)
     return df
